a58031a16cd8cc30af1ce33063b624344ed5a5de
Abraham Piperidis <piperidis3@yahoo.gr>
2015-08-01 00:02:15 +0300

    jni

--- RemoteClientFx/src/main/java/RemoteApp/JNI/KillProcCplusplus.java
+++ RemoteClientFx/src/main/java/RemoteApp/JNI/KillProcCplusplus.java
@@ -10,15 +10,25 @@ public class KillProcCplusplus {
     
     static {
         id = ManagementFactory.getRuntimeMXBean().getName();
+        try {
         System.loadLibrary("RemoteClientFx");
+        } catch(UnsatisfiedLinkError  ex) {
+            
+        } finally {
+            
+        }
     }
     
     private static native void killJavaProcess(int pid);
     
     public static void killJavaProcessJni() {
-        
+        try {
         killJavaProcess(Integer.valueOf(id.substring(0,id.indexOf("@"))));
-        
+        } catch(UnsatisfiedLinkError ex) {
+            
+        } finally {
+            
+        }
         
     }
     
