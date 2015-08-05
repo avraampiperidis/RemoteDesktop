8143832ddc4be4b92bad1b3a3c5f7893a531483f
Abraham Piperidis <piperidis3@yahoo.gr>
2015-08-05 06:51:25 +0300

    constants key interface

---
+++ RemoteClientFx/src/main/java/RemoteApp/KeyInterface.java
@@ -0,0 +1,11 @@
+
+package RemoteApp;
+
+
+public interface KeyInterface {
+    
+    public static final String ALGORITHM = "AES";
+    public static final String TRANSFORMATION = "AES";
+    public static final String KEY = "kdif(34&4']qvDF@";
+    
+}
